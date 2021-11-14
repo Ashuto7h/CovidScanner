5e62a09da91b3f3405ab047c6f1e82a9d66f235e
Ashuto7h <ashusahu198@gmail.com>
2021-11-14 20:08:12 +0530

    changed mkdir

--- frontend/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import CustomAppBar from './CustomAppBar';
-import Dropzone from './Dropzone';
-import FadeIn from './FadeIn';
-import Footer from './Footer';
-import PredictCard from './PredictCard';
-import PredictDialog from './PredictDialog';
-import UploadCard from './UploadCard';
-
-export { CustomAppBar, Dropzone, FadeIn, Footer, PredictCard, PredictDialog, UploadCard };
--- frontend/src/constants/theme.js
+++
@@ -1,9 +0,0 @@
-export const COLORS = {
-    appBarBackground: '#010739',
-    covidColor: '#6abbff',
-    lightBackgroundDrop: '#023574',
-    madeByText: '#87b1ff',
-    scannerColor: '#bfd1e3',
-    uploadLightColor: '#04123acc',
-    white: '#fff'
-};
